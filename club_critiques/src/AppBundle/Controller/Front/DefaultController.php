5eb140f736bb90f311e2facdd391ee187bdd070e
Simon Humbert <shumbert@kernix.com>
2017-04-12 12:08:12 +0200

    Add admin + css + base directories

--- club_critiques/src/AppBundle/Controller/Front/DefaultController.php
+++ club_critiques/src/AppBundle/Controller/Front/DefaultController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace AppBundle\Controller;
+namespace AppBundle\Controller\Front;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
@@ -15,6 +15,17 @@ class DefaultController extends Controller
     {
         // replace this example code with whatever you need
         return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+        ]);
+    }
+
+    /**
+     * @Route("/contenus", name="contenus")
+     */
+    public function contenusActions (Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/contenus.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
         ]);
     }
