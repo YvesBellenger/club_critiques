fb64cd43ea89941bf71898e3bb400a3e25adb5a1
Jérémy Moreau <jeremyfs.moreau@gmail.com>
2017-06-28 21:34:27 +0200

    salons

--- club_critiques/src/AppBundle/Controller/Front/SalonsController.php
+++ club_critiques/src/AppBundle/Controller/Front/SalonsController.php
@@ -5,19 +5,30 @@ namespace AppBundle\Controller\Front;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\HttpFoundation\Response;
 
 class SalonsController extends Controller
 {
     /**
-     * @Route("/", name="salons")
+     * @Route("/salons/salon/exemple", name="salon")
      */
 
-    public function salonsAction(Request $request)
+    public function salonAction(Request $request)
+    {
+        return $this->render('salons/salon.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+            'controller' => 'salon'
+        ]);
+    }
+
+    /**
+     * @Route("/salons/liste", name="salons-liste")
+     */
+
+    public function salonListeAction(Request $request)
     {
         return $this->render('salons/salons-list.html.twig', [
             'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-            'controller' => 'salons',
+            'controller' => 'salons-liste'
         ]);
     }
 }
