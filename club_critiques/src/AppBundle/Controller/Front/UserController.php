97da5faa0bbe489078e32d2ff919b09dfbf34ff1
Simon Humbert <shumbert@kernix.com>
2017-07-07 11:28:59 +0200

    Remove comments

--- club_critiques/src/AppBundle/Controller/Front/UserController.php
+++ club_critiques/src/AppBundle/Controller/Front/UserController.php
@@ -15,28 +15,16 @@ class UserController extends Controller
     {
         $doctrine = $this->getDoctrine();
         $user = $doctrine->getRepository('AppBundle:User')->find($request->get('id'));
-
-        // Create the form according to the FormType created previously.
-        // And give the proper parameters
         $form = $this->createForm('AppBundle\Form\ContactType',null,array(
-            // To set the action use $this->generateUrl('route_identifier')
-            //'action' => $this->generateUrl('myapplication_contact'),
             'method' => 'POST'
         ));
 
         if ($request->isMethod('POST')) {
-            // Refill the fields in case the form is not valid.
             $form->handleRequest($request);
-
             if($form->isValid()){
-                // Send mail
                 if($this->sendEmail($form->getData())){
-
-                    // Everything OK, redirect to wherever you want ! :
-
                     return $this->redirectToRoute('redirect_to_somewhere_now');
                 }else{
-                    // An error ocurred, handle
                     var_dump("Errooooor :(");
                 }
             }
