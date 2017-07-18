636813043ed95794fd13dd1d8327ddef62c42a0b
yves <yvesbellenger@hotmail.com>
2017-07-19 01:25:55 +0200

    final push

---
+++ club_critiques/web/js/lobby-propose.js
@@ -0,0 +1,7 @@
+// $(function() {
+//
+//     var lobby_propose = $('#lobby-propose');
+//
+//     lobby_propose.find('#lobby_date_start').find('select').addclass('form-control');
+//     lobby_propose.find('#lobby_date_end').find('select').addclass('form-control');
+// });
--- club_critiques/web/js/menu.js
+++ club_critiques/web/js/menu.js
@@ -10,11 +10,6 @@ $(function() {
         var scrollTop = $window.scrollTop();
         navbar.toggleClass('hide-menu', scrollTop > prev);
         $('.float-menu').toggleClass('fix-top', scrollTop > prev);
-        // $('.header').toggleClass('fix-top', scrollTop > prev);
-        //
-        // $('.fix-top').animate({
-        //     top: "0"
-        // }, 1000);
 
         prev = scrollTop;
     });
