55848c0858bc6aafe464e4baefddb0b1c48ebc52
Simon Humbert <shumbert@kernix.com>
2017-07-18 15:56:26 +0200

    Clean comments

--- club_critiques/src/AppBundle/Admin/LobbyAdmin.php
+++ club_critiques/src/AppBundle/Admin/LobbyAdmin.php
@@ -17,12 +17,9 @@ class LobbyAdmin extends AbstractAdmin
      * @var array
      */
     protected $datagridValues = array(
-        '_page' => 1,            // display the first page (default = 1)
-        '_sort_order' => 'DESC', // reverse order (default = 'ASC')
-        '_sort_by' => 'createdByUser'  // name of the ordered field
-        // (default = the model's id field, if any)
-
-        // the '_sort_by' key can be of the form 'mySubModel.mySubSubModel.myField'.
+        '_page' => 1,
+        '_sort_order' => 'DESC',
+        '_sort_by' => 'createdByUser'
     );
 
 
